d338782f78311b7c0f365b35a20ed000ee5aa184
totte <totte@LAPTOP-BRSMDCPF>
2020-05-29 01:00:31 -0300

    Creador: Antonela Flores

---
+++ src/main/java/com/example/demo/FloresPerez3863Tp5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresPerez3863Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresPerez3863Tp5Application.class, args);
+	}
+
+}
